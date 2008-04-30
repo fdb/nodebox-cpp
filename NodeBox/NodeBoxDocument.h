00647d61794f0b380b5bcdcee0721b191b6a9077
Frederik De Bleser <frederik@pandora.be>
2008-04-30 10:52:26 +0200

    NodeBox intial app skeleton.

---
+++ NodeBox/NodeBoxDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  NodeBox
+//
+//  Created by Frederik on 30/04/08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface NodeBoxDocument : NSDocument
+{
+}
+@end
---
+++ NodeBox/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NodeBox
+//
+//  Created by Frederik on 30/04/08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
