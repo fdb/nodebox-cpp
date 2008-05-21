4c8a99e685b84f4a4dabd2e72edb7e41ebdccd8f
Frederik De Bleser <frederik@pandora.be>
2008-05-21 12:11:41 +0200

    Initial CoreVector codebase.

---
+++ CoreVector/CoreVectorLibrary.cpp
@@ -0,0 +1,32 @@
+/*
+ * This file is part of NodeBox.
+ *
+ * Copyright (C) 2008 Frederik De Bleser (frederik@pandora.be)
+ *
+ * NodeBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NodeBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NodeBox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CoreVector.h"
+#include "RectNode.h"
+
+using namespace CoreVector;
+
+Node* createRectNode() {
+    return new RectNode();
+}
+
+extern "C" void node_library_info(NodeLibrary& lib)
+{
+    lib.addNodeInfo("RectNode", createRectNode);
+}
---
+++ CoreVector/test.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of NodeBox.
+ *
+ * Copyright (C) 2008 Frederik De Bleser (frederik@pandora.be)
+ *
+ * NodeBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NodeBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NodeBox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <iostream>
+#include <NodeCore/NodeCore.h>
+ 
+using namespace NodeCore;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    NodeLibraryManager *manager = new NodeLibraryManager();
+    NodeLibrary *coreVectorLibrary = manager->loadLatest("corevector");
+    Node *rectNode = coreVectorLibrary->createNode("RectNode");
+    rectNode->set("x", 10);
+    rectNode->set("y", 10);
+    rectNode->set("width", 10);
+    rectNode->set("height", 10);
+    Canvas *c = (Canvas *)rectNode->outputAsData();
+    c->save("test.pdf");
+	cout << "Wrote test.pdf." << endl;
+}
