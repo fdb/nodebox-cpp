3a20428997ecf7bb9488871d71b9b54cc03c13f1
Frederik De Bleser <frederik@pandora.be>
2008-06-05 12:34:39 +0200

    NXFieldBox added that draws the background in ParameterView.

--- NodeBox/ParameterViewController.mm
+++ NodeBox/ParameterViewController.mm
@@ -20,6 +20,7 @@
 #import "ParameterViewController.h"
 #import "NodeBoxWindowController.h"
 #import "NXDragControl.h"
+#import "NXFieldBox.h"
 
 //static NSFont *kSmallFont = [NSFont fontWithName:@"LucidaGrande-Bold" size:10];
 
@@ -28,8 +29,9 @@
 - (void)setValueForControls;
 - (void)removeControls;
 - (void)rebuildControls;
-- (void)addLabelForField:(NodeCore::Field *)field y:(float)y index:(int)index;
-- (NXDragControl *)addFloatControlForField:(NodeCore::Field *)field y:(float)y index:(int)index;
+- (NXFieldBox*)addBoxForField:(NodeCore::Field *)field y:(float)y index:(int)index;
+- (void)addLabelForField:(NodeCore::Field *)field y:(float)y index:(int)index toBox:(NXFieldBox *)box;
+- (NXDragControl *)addFloatControlForField:(NodeCore::Field *)field y:(float)y index:(int)index toBox:(NXFieldBox *)box;
 - (void)floatValueChanged:(id)sender;
 
 @end
@@ -101,47 +103,57 @@
     [self removeControls];
     if (![self activeNode]) return;
     NSRect frame = [_view frame];
-    float y = 5;
+    float y = frame.size.height - 24;
     int index = 0;
     NodeCore::FieldList fields = [self activeNode]->getFields();
     for (NodeCore::FieldIterator iter = fields.begin(); iter != fields.end(); ++iter) {
         NodeCore::Field *field = *iter;
         NSString *fieldName = [NSString stringWithUTF8String:field->getName().c_str()];
         if (field->getType() == NodeCore::kFloat) {
-            [self addLabelForField:field y:y index:index];
-            NSControl *control = [self addFloatControlForField:field y:y index:index];
+            NXFieldBox *fieldBox = [self addBoxForField:field y:y index:index];
+            [self addLabelForField:field y:y index:index toBox:fieldBox];
+            NSControl *control = [self addFloatControlForField:field y:y index:index toBox:fieldBox];
             [_controlMap setObject:control forKey:fieldName];
+            y -= 24;
         } else {
             NSLog(@"Field: %s: don't know how to handle type %s", field->getName().c_str(), field->getType().c_str());
         }
-        y += 21;
         index++;
     }
 }
 
-- (void)addLabelForField:(NodeCore::Field *)field y:(float)y index:(int)index
+- (NXFieldBox*)addBoxForField:(NodeCore::Field *)field y:(float)y index:(int)index
+{
+    NSRect fieldBoxRect = NSMakeRect(0, y, [_view frame].size.width, 24);
+    NXFieldBox *fieldBox = [[NXFieldBox alloc] initWithFrame:fieldBoxRect];
+    [fieldBox setAutoresizingMask:NSViewWidthSizable |NSViewMinYMargin];
+    [_view addSubview:fieldBox];
+    return fieldBox;
+}
+
+- (void)addLabelForField:(NodeCore::Field *)field y:(float)y index:(int)index toBox:(NXFieldBox *)box
 {
     NSTextField *label = [[NSTextField alloc] init];
-    [label setFrame:NSMakeRect(0, y, 100, 15)];
+    [label setFrame:NSMakeRect(0, 4, 80, 15)];
     [label setStringValue:[NSString stringWithUTF8String:field->getName().c_str()]];
     [label setAlignment:NSRightTextAlignment];
     [label setEditable:FALSE];
     [label setBordered:FALSE];
     [label setDrawsBackground:FALSE];
     [label setFont:[NSFont fontWithName:@"LucidaGrande-Bold" size:10]];
-    [_view addSubview:label];
+    [box addSubview:label];
 }
 
 
-- (NXDragControl *)addFloatControlForField:(NodeCore::Field *)field y:(float)y index:(int)index
+- (NXDragControl *)addFloatControlForField:(NodeCore::Field *)field y:(float)y index:(int)index toBox:(NXFieldBox *)box
 {
     NXDragControl *control = [[NXDragControl alloc] init];
-    [control setFrame:NSMakeRect(108, y-1, 100, 15)];
+    [control setFrame:NSMakeRect(108, 4, 70, 15)];
     [control setFloatValue:field->asFloat()];
     [control setTarget:self];
     [control setTag:index];
     [control setAction:@selector(floatValueChanged:)];
-    [_view addSubview:control];
+    [box addSubview:control];
     return control;
 }
 
